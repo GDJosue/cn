d2d20645181ffcacb95d5f364b5aa6dbac79d97e
Josue Arellano Castro <86698612+GDJosue@users.noreply.github.com>
2023-11-01 12:54:19 -0600

    Update db.php
    Seguridad

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 class Database{
     public static function connect(){
         $hostname_localhost ="localhost";
-		$database_localhost ="u750169748_cxqwL";
-		$username_localhost ="u750169748_cSG7t";
-		$password_localhost ="CNcc1805#";
+		$database_localhost ="user";
+		$username_localhost ="user";
+		$password_localhost ="password";
         $db = new mysqli($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
         $db -> query("SET NAMES 'utf8'");
         return $db; 
